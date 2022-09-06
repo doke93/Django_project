314c4b87c5fc36724dc2461cb86ac01527f0c621
Doke <dokeabhishek3@gmail.com>
2022-09-06 21:09:30 +0530

    second upload

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,39 @@
+from email import message
+from urllib import request
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-def projects(requets):
-    return render(requets, 'projects.html')
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
 
-def project(requets, pk):
-    return HttpResponse('Single project' +' '+str(pk))
+
+# Create your views here.
+def projects(request):
+    page = 'project'
+    number = 10
+    context = {'page':page, 'number':number, 'projects':projectsList}
+    return render(request, 'projects/projects.html', context)
+
+def project(request, pk):
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+
+    context = {'project':projectObj}
+    return render(request,'projects/single-project.html', context)
